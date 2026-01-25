264ed56f1c7454109a71e39d542bfbb9f00a3117
Probal Ghosh <probalghoshofficial@gmail.com>
2026-01-25 15:24:41 +0530

    Add authentication with next-auth and Google provider; update dependencies

---
+++ src/app/login/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { signIn, useSession } from "next-auth/react";
+
+const Page = () => {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
+  if (!session) {
+    return (
+      <div>
+        <button
+          onClick={() => signIn("google")}
+        >
+          Sign in with Google
+        </button>
+      </div>
+    );
+  }
+
+  if(session){
+    console.log(session)
+  }
+
+  return (
+    <div>
+      <p>Logged in as {session.user?.email}</p>
+    </div>
+  );
+};
+
+export default Page;
